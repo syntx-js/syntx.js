1c71696980c179527deef69ce5addba38867fb0f
Uxoi <jhojanproxdd@gmail.com>
2024-08-12 15:59:23 -0500

    Export all functions.

---
+++ src/index.js
@@ -0,0 +1,53 @@
+const discord = require("discord.js");
+const ERXClient = require("./classes/client");
+const Embed = require("./classes/embed")
+const { Var } = require("./classes/variables");
+const text = require("./functions/random/text");
+const number = require("./functions/random/number");
+const intents = require("./intents/intents");
+const argument = require("./functions/message/argument");
+const send = require("./functions/message/sendMessage");
+const clientId = require("./functions/client/id");
+const username = require("./functions/user/username")
+const authorId = require("./functions/user/authorId");
+const avatar = require("./functions/user/avatar");
+const mentioned = require("./functions/message/mentioned");
+const guildId = require("./functions/guild/id")
+const say = require("./functions/message/say");
+const data = require("./functions/message/data");
+const channelId = require("./functions/channel/id")
+
+module.exports = {
+    ...discord,
+    ERXClient,
+    Intents: intents,
+    Embed,
+    Var,
+    cmd: {
+        random: {
+            text,
+            number
+        },
+        channeL: {
+            id: channelId
+        },
+        message: {
+            argument,
+            send,
+            mentioned,
+            say,
+            data
+        },
+        client: {
+            id: clientId
+        },
+        user: {
+            username,
+            authorId,
+            avatar
+        },
+        guild: {
+            id: guildId
+        }
+    }
+};
