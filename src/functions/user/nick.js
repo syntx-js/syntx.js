9000045e78d1e3e6daeb7d7589dfb4880b1c4ffb
Uxoi <jhojanproxdd@gmail.com>
2024-08-18 12:52:31 -0500

    Added, allows you to view the user's nickname on the provided server

---
+++ src/functions/user/nick.js
@@ -0,0 +1,13 @@
+module.exports = async function nick(id, guildID, client) {
+    if (!client || !guildID) {
+        throw new Error("Client and guild must be defined.");
+    }
+
+    try {
+        const guild = await client.bot.guilds.fetch(guildID);
+        const member = await guild.members.fetch(id);
+        return member.nickname || member.user.username;
+    } catch (error) {
+        throw new Error("Could not fetch user or guild.");
+    }
+}
