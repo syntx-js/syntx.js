62eb96cc87f2cc4951b0908111853acd325d048f
Uxoi <jhojanproxdd@gmail.com>
2024-08-17 23:40:25 -0500

    Update perms.js

--- src/functions/user/perms.js
+++ src/functions/user/perms.js
@@ -1,58 +1,68 @@
-module.exports = function perms({ userID, permission }, message) {
-    const permissions = [
-        'CREATE_INSTANT_INVITE',
-        'KICK_MEMBERS',
-        'BAN_MEMBERS',
-        'ADMINISTRATOR',
-        'MANAGE_CHANNELS',
-        'MANAGE_GUILD',
-        'ADD_REACTIONS',
-        'VIEW_AUDIT_LOG',
-        'PRIORITY_SPEAKER',
-        'STREAM',
-        'VIEW_CHANNEL',
-        'SEND_MESSAGES',
-        'SEND_TTS_MESSAGES',
-        'MANAGE_MESSAGES',
-        'EMBED_LINKS',
-        'ATTACH_FILES',
-        'READ_MESSAGE_HISTORY',
-        'MENTION_EVERYONE',
-        'USE_EXTERNAL_EMOJIS',
-        'VIEW_GUILD_INSIGHTS',
-        'CONNECT',
-        'SPEAK',
-        'MUTE_MEMBERS',
-        'DEAFEN_MEMBERS',
-        'MOVE_MEMBERS',
-        'USE_VAD',
-        'CHANGE_NICKNAME',
-        'MANAGE_NICKNAMES',
-        'MANAGE_ROLES',
-        'MANAGE_WEBHOOKS',
-        'MANAGE_EMOJIS_AND_STICKERS',
-        'USE_APPLICATION_COMMANDS',
-        'REQUEST_TO_SPEAK',
-        'MANAGE_THREADS',
-        'USE_PUBLIC_THREADS',
-        'USE_PRIVATE_THREADS',
-        'SEND_MESSAGES_IN_THREADS',
-        'START_EMBEDDED_ACTIVITIES'
-    ];
+const { PermissionsBitField } = require('discord.js');
 
+const PERMISSIONS_MAP = {
+    "CREATE_INSTANT_INVITE": "CreateInstantInvite",
+    "KICK_MEMBERS": "KickMembers",
+    "BAN_MEMBERS": "BanMembers",
+    "ADMINISTRATOR": "Administrator",
+    "MANAGE_CHANNELS": "ManageChannels",
+    "MANAGE_GUILD": "ManageGuild",
+    "ADD_REACTIONS": "AddReactions",
+    "VIEW_AUDIT_LOG": "ViewAuditLog",
+    "PRIORITY_SPEAKER": "PrioritySpeaker",
+    "STREAM": "Stream",
+    "VIEW_CHANNEL": "ViewChannel",
+    "SEND_MESSAGES": "SendMessages",
+    "SEND_TTS_MESSAGES": "SendTTSMessages",
+    "MANAGE_MESSAGES": "ManageMessages",
+    "EMBED_LINKS": "EmbedLinks",
+    "ATTACH_FILES": "AttachFiles",
+    "READ_MESSAGE_HISTORY": "ReadMessageHistory",
+    "MENTION_EVERYONE": "MentionEveryone",
+    "USE_EXTERNAL_EMOJIS": "UseExternalEmojis",
+    "VIEW_GUILD_INSIGHTS": "ViewGuildInsights",
+    "CONNECT": "Connect",
+    "SPEAK": "Speak",
+    "MUTE_MEMBERS": "MuteMembers",
+    "DEAFEN_MEMBERS": "DeafenMembers",
+    "MOVE_MEMBERS": "MoveMembers",
+    "USE_VAD": "UseVAD",
+    "CHANGE_NICKNAME": "ChangeNickname",
+    "MANAGE_NICKNAMES": "ManageNicknames",
+    "MANAGE_ROLES": "ManageRoles",
+    "MANAGE_WEBHOOKS": "ManageWebhooks",
+    "MANAGE_EMOJIS_AND_STICKERS": "ManageEmojisAndStickers",
+    "USE_APPLICATION_COMMANDS": "UseApplicationCommands",
+    "REQUEST_TO_SPEAK": "RequestToSpeak",
+    "MANAGE_THREADS": "ManageThreads",
+    "CREATE_PUBLIC_THREADS": "CreatePublicThreads",
+    "CREATE_PRIVATE_THREADS": "CreatePrivateThreads",
+    "USE_EXTERNAL_STICKERS": "UseExternalStickers",
+    "SEND_MESSAGES_IN_THREADS": "SendMessagesInThreads",
+    "USE_EMBEDDED_ACTIVITIES": "UseEmbeddedActivities",
+    "MODERATE_MEMBERS": "ModerateMembers"
+};
+
+module.exports = async function perms({ userID, permission }, message) {
     if (!userID || typeof userID !== 'string' || !permission || typeof permission !== 'string') {
         throw new Error('"userID" or "permission" is not a valid string.');
     }
 
-    if (!permissions.includes(permission)) {
-        throw new Error(`The permission "${permission}" is not valid.`);
+    const permissionKey = PERMISSIONS_MAP[permission];
+
+    if (!permissionKey) {
+        throw new Error('The permission provided is not valid.');
     }
 
-    const member = message.guild.members.cache.get(userID);
+    try {
+        const member = await message.guild.members.fetch(userID);
 
-    if (!member) {
-        throw new Error("The user was not found.");
+        if (!member) {
+            throw new Error("The user was not found.");
+        }
+
+        return member.permissions.has(PermissionsBitField.Flags[permissionKey]);
+    } catch (error) {
+        throw new Error("There was an issue fetching the member or checking permissions " + error);
     }
-
-    return member.permissions.has(permission);
 };
