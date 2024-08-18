40359bb1642e96d1c0f49b5ac032b2462f4747e1
Uxoi <jhojanproxdd@gmail.com>
2024-08-18 13:54:59 -0500

    Updated perms.js: can now count the first argument of the message, this if 'firstArgument' is true

--- src/functions/message/content.js
+++ src/functions/message/content.js
@@ -1,7 +1,9 @@
-module.exports = function content(message) {
+module.exports = function content(message, firstArgument = false) {
     if (message && message.content) {
         const words = message.content.split(' ');
-        const result = words.slice(1).join(' ');
+        
+        const result = firstArgument ? words.join(' ') : words.slice(1).join(' ');
+        
         return result;
     }
     
---
+++ src/functions/message/edit.js
@@ -0,0 +1,31 @@
+module.exports = async function edit({ data: { channel, message }, content }, client) {
+    if (typeof channel !== 'string') {
+        throw new TypeError('Channel must be a string.');
+    }
+
+    if (typeof message !== 'string') {
+        throw new TypeError('Message ID must be a string.');
+    }
+
+    if (typeof content !== 'string') {
+        throw new TypeError('Content must be a string.');
+    }
+
+    const targetChannel = client.channels.cache.get(channel.toString());
+    
+    if (!targetChannel) {
+        throw new Error('Channel not found.');
+    }
+
+    const targetMessage = await targetChannel.messages.fetch(message.toString());
+
+    if (!targetMessage) {
+        throw new Error('Message not found.');
+    }
+
+    if (targetMessage.author.id !== client.user.id) {
+        throw new Error('You can only edit messages sent by the bot.');
+    }
+
+    await targetMessage.edit(content);
+};
