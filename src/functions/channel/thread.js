f6b060c758836734795503eff30e15092ec83785
Uxoi <jhojanproxdd@gmail.com>
2024-11-19 22:01:38 -0500

    [thread.js] We have added an option to send a message in the thread after it has been created, as well as being able to choose between GUILD_PUBLIC_THREAD and GUILD_PRIVATE_THREAD

--- src/functions/channel/thread.js
+++ src/functions/channel/thread.js
@@ -1,6 +1,6 @@
 const { ChannelType, ThreadAutoArchiveDuration } = require('discord.js');
 
-module.exports = async function thread({ channelID, messageID, name, duration, returnID = false }, client) {
+module.exports = async function thread({ channelID, messageID, name, duration, type = 'GUILD_PUBLIC_THREAD', returnID = false, content = null }, client) {
     if (!client) throw new TypeError('Client is required.');
     if (!channelID || !messageID || !name || !duration) throw new TypeError('Invalid arguments provided.');
 
@@ -16,9 +16,23 @@ module.exports = async function thread({ channelID, messageID, name, duration, r
     const thread = await message.startThread({
         name: name,
         autoArchiveDuration: duration,
+        type,
     });
 
     if (!thread) throw new Error('Failed to create thread.');
+
+    if (content) {
+        if (typeof content === 'string') {
+            await thread.send({
+                content: content
+            });
+        }
+        else if (content.embeds) {
+            await thread.send({
+                embeds: content.embeds
+            });
+        }
+    }
 
     return returnID ? thread.id : undefined;
 };
